ea36fe34d8567d15234615aacbcb90912f7d01a8
unknown <tristan@.(none)>
2010-11-16 01:23:44 -0500

    Added document term frequency count to index.

---
+++ Spimi/Posting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concordia.Spimi
+{
+    [Serializable]
+    class Posting
+    {
+        public string DocumentId { get; set; }
+        public int Frequency { get; set; }
+
+        public Posting(string documentId, int frequency)
+        {
+            this.DocumentId = documentId;
+            this.Frequency = frequency;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Posting other = obj as Posting;
+            return other != null
+                && other.DocumentId.Equals(this.DocumentId)
+                && other.Frequency.Equals(this.Frequency);
+        }
+
+        public override string ToString()
+        {
+            return "<"+this.DocumentId+", "+this.Frequency+">";
+        }
+    }
+}
---
+++ SpimiTests/BinaryReaderTestHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Concordia.Spimi;
+
+namespace Concordia.SpimiTests
+{
+    class BinaryReaderTestHelper
+    {
+        public static Posting readPosting(BinaryReader reader)
+        {
+            return new Posting(reader.ReadString(), reader.ReadInt32());
+        }
+    }
+}
